c0c940226f592e25e8fe7543b0aa2a42aee3449f
gksruf2848 <4455qqq@naver.com>
2022-05-09 15:06:00 +0900

    .

--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -1,4 +1,18 @@
 package LinkedList;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        Node<?> n1 = Util.makeSet('a');
+        Node<?> n2 = Util.makeSet('b');
+        Node<?> n3 = Util.makeSet('c');
+
+
+
+
+
+
+    }
+
 }
--- src/LinkedList/Util.java
+++ src/LinkedList/Util.java
@@ -2,19 +2,27 @@ package LinkedList;
 
 public class Util {
 
+    //Make-Set(data)
     public static <T> Node<T> makeSet(T data) {
         return new Node<>(data);
     }
 
+    //Find-Set(node) : node가 속한 대장 head를 알아냅니다.
     public static <T> Node<T> findSet(Node<T> node) {
-        if(node.headNode == node) {
-            return node;
+        return node.headNode; //그냥 모든 노드에는 대장 head에 대한 정보가 headNode에 담겨 있으므로 그것을 리턴합니다.
+
+        /*
+        if(node.headNode == node) {     //만약 해당 노드의 headNode가 자기 자신이라면, 본인이 대장 head라는 뜻이므로
+            return node;                //자기 자신을 리턴합니다.
         }
         else return findSet(node.headNode);
+        */
+
     }
 
+    //Union(X, Y) : node X가 속한 집합과, Y가 속한 집합을 합칩니다.
     public static <T> void union(Node<T> n1, Node<T> n2) {
-        if(findSet(n1) != findSet(n2)) {
+        if(findSet(n1) != findSet(n2)) { //
             changeHead(n1.headNode, n2.headNode);
             findTail(n1).nextNode = findSet(n2);
         }
